bbf25583558db6c8983373f203b88593230d145c
Ricardo Marques <ricardomarques377@gmail.com>
2020-03-21 13:16:19 +0000

    shebang

--- backend/db-test.sh
+++ backend/db-test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export HOST="${DB_HOST:-localhost}"
 export POSTGRES_URL=postgres://postgres:mysecretpassword@$HOST:5432/somethingoftheday?sslmode=disable
 go test -count=1 -v ./datastore/...
