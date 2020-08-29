c551de7361794e78a2d991a274968d782ce59925
Ricardo Marques <ricardomarques377@gmail.com>
2020-08-29 17:10:18 +0100

    ssm params (#5)
    * ssm params

--- terraform/aws/output.tf
+++ terraform/aws/output.tf
@@ -5,16 +5,16 @@ output "ecr_repository_url" {
 output "container_definition" {
   value = templatefile("${path.module}/container_definition.json", {
     repository_url        = data.aws_ecr_repository.sotd-repo.repository_url,
-    db_url                = aws_secretsmanager_secret.sotd_db_url.arn,
-    twitter_client_id     = aws_secretsmanager_secret.sotd_twitter_client_id.arn,
-    twitter_client_secret = aws_secretsmanager_secret.sotd_twitter_client_secret.arn
+    db_url                = aws_ssm_parameter.sotd_db_url.arn,
+    twitter_client_id     = aws_ssm_parameter.sotd_twitter_client_id.arn,
+    twitter_client_secret = aws_ssm_parameter.sotd_twitter_client_secret.arn
   })
 }
 
 output "secrets_arns" {
   value = [
-    aws_secretsmanager_secret.sotd_db_url.arn,
-    aws_secretsmanager_secret.sotd_twitter_client_id.arn,
-    aws_secretsmanager_secret.sotd_twitter_client_secret.arn
+    aws_ssm_parameter.sotd_db_url.arn,
+    aws_ssm_parameter.sotd_twitter_client_id.arn,
+    aws_ssm_parameter.sotd_twitter_client_secret.arn
   ]
 }
--- terraform/aws/secrets.tf
+++ terraform/aws/secrets.tf
@@ -1,26 +1,32 @@
-resource "aws_secretsmanager_secret" "sotd_db_url" {
-  name = "db-url"
+resource "aws_ssm_parameter" "sotd_db_url" {
+  name        = "/production/sotd/database/db_url"
+  description = "PG connection URL"
+  type        = "SecureString"
+  value       = var.postgres_url
+
+  tags = {
+    environment = "production"
+  }
 }
 
-resource "aws_secretsmanager_secret_version" "sotd_db_url_value" {
-  secret_id     = aws_secretsmanager_secret.sotd_db_url.id
-  secret_string = var.postgres_url
+resource "aws_ssm_parameter" "sotd_twitter_client_id" {
+  name        = "/production/sotd/database/twitter_client_id"
+  description = "Twitter Client ID"
+  type        = "String"
+  value       = var.twitter_client_id
+
+  tags = {
+    environment = "production"
+  }
 }
 
-resource "aws_secretsmanager_secret" "sotd_twitter_client_id" {
-  name = "twitter_client_id"
-}
+resource "aws_ssm_parameter" "sotd_twitter_client_secret" {
+  name        = "/production/sotd/database/twitter_client_secret"
+  description = "Twitter Client Secret"
+  type        = "SecureString"
+  value       = var.twitter_client_secret
 
-resource "aws_secretsmanager_secret_version" "sotd_twitter_client_id_value" {
-  secret_id     = aws_secretsmanager_secret.sotd_twitter_client_id.id
-  secret_string = var.twitter_client_id
-}
-
-resource "aws_secretsmanager_secret" "sotd_twitter_client_secret" {
-  name = "twitter_client_secret"
-}
-
-resource "aws_secretsmanager_secret_version" "sotd_twitter_client_secret_value" {
-  secret_id     = aws_secretsmanager_secret.sotd_twitter_client_secret.id
-  secret_string = var.twitter_client_secret
+  tags = {
+    environment = "production"
+  }
 }
