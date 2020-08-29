ac3090c4b951412db7ae50b61b3536b3760d5e9f
Ricardo Marques <ricardomarques377@gmail.com>
2020-08-29 19:14:39 +0100

    terraform improvement

--- terraform/aws/ecr-repository.tf
+++ terraform/aws/ecr-repository.tf
@@ -2,7 +2,3 @@ resource "aws_ecr_repository" "sotd-repo" {
   name                 = "something-of-the-day"
   image_tag_mutability = "MUTABLE"
 }
-
-data "aws_ecr_repository" "sotd-repo" {
-  name = aws_ecr_repository.sotd-repo.name
-}
--- terraform/aws/output.tf
+++ terraform/aws/output.tf
@@ -4,7 +4,7 @@ output "ecr_repository_url" {
 
 output "container_definition" {
   value = templatefile("${path.module}/container_definition.json", {
-    repository_url        = data.aws_ecr_repository.sotd-repo.repository_url,
+    repository_url        = aws_ecr_repository.sotd-repo.repository_url,
     db_url                = aws_ssm_parameter.sotd_db_url.arn,
     twitter_client_id     = aws_ssm_parameter.sotd_twitter_client_id.arn,
     twitter_client_secret = aws_ssm_parameter.sotd_twitter_client_secret.arn
